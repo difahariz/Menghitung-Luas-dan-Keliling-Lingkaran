52e536ed34e776dc57893b9d130f0302b3d061c8
Difa Hariz <difahariz@gmail.com>
2021-10-28 17:05:12 +0700

    Menambahkan File

---
+++ Menghitung_luas_dan_keliling_lingkaran.py
@@ -0,0 +1,12 @@
+phi = 3.14
+
+r = float(input("Masukan jari-jari lingkaran : "))
+
+luas = phi*(r*r)
+keliling = 2*phi*r
+print ()
+print ("Rumus Luas Lingkarannya \t=",phi,"x",r,"x",r,)
+print ("Rumus Keliling Lingkaran \t=",2,"x",phi,"x",r,)
+print ()
+print ("Hasil Luas Lingkaran \t\t= ",format(luas,'.2f'))
+print ("Hasil Keliling Lingkaran\t= ",format(keliling,'.2f'))
